c6530e6ea7a9326981faba676017a9c524d40181
humaira228 <167327288+humaira228@users.noreply.github.com>
2025-05-16 07:11:44 +0600

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
+using LL = long long;
+
+
+
+void pre()
+{
+    fastio;
+
+    
+}
+
+
+
+void solve(int tc)
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+
+    }
+    sort(v.begin(),v.end());
+    int cnt=0;
+
+    for(int i=0; i<n && x>0; i++){
+        if(x>=v[i]){
+        x-=v[i];
+        cnt++;}
+        else break;
+
+    }
+    cout<<cnt;
+
+
+
+}
+
+signed main()
+{
+    pre();
+
+    int tc, tt = 1;
+     cin >> tt;
+    
+    for(tc = 1; tc <= tt; tc++)
+    {
+        // cout << "Case " << tc << ": ";
+        solve(tc);
+        // cout << '\n';
+    }
+
+    return 0;
+}
