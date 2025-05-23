e3cdf1ce347a32e5467e50052f1212121e8131b9
humaira228 <167327288+humaira228@users.noreply.github.com>
2025-05-23 07:54:31 +0600

    Update lab2t3.cpp

--- lab2t3.cpp
+++ lab2t3.cpp
@@ -7,29 +7,36 @@ using LL = long long;
 void solve() {
     int n;
     cin >> n;
-
-    vector<LL> s(n), e(n);
-    for (int i = 0; i < n; i++) cin >> s[i];
-    for (int i = 0; i < n; i++) cin >> e[i];
-
-    vector<pair<LL, LL>> activities(n);
-    for (int i = 0; i < n; i++) {
-        activities[i] = {e[i], s[i]};  // sort by end time
+    
+    vector<LL> s(n),e(n);
+    for(int i=0; i<n; i++){
+        cin>> s[i];
+    }
+    for(int i=0; i<n; i++){
+        cin>> e[i];
+    }
+    
+    vector<pair<LL,LL>>activity(n);
+    for(int i=0; i<n; i++){
+        activity[i]={e[i],s[i]};
     }
 
-    sort(activities.begin(), activities.end());
+    sort(activity.begin(), activity.end());
+    int c=0;
 
-    int count = 0;
-    LL last_end = -1;
+    LL lastEnd=-1;
 
-    for (const auto& [end, start] : activities) {
-        if (start >= last_end) {
-            count++;
-            last_end = end;
+    for(auto& p:activity){
+        LL end= p.first;
+        LL start= p.second;
+        if(start>=lastEnd){
+            c++;
+            lastEnd=end;
         }
     }
+    cout<<c;
 
-    cout << count;
+    
 }
 
 int main() {
